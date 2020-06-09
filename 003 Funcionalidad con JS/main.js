ff1c89bc0a2a6be6938dedbc2b1c8bee5e65c766
Erineo de León <60766878+erineodeleon@users.noreply.github.com>
2020-06-09 06:47:18 -0600

    Revert "Revert "K borge""

--- 003 Funcionalidad con JS/main.js
+++ 003 Funcionalidad con JS/main.js
@@ -1,9 +1,19 @@
  // Esta seria la funcion para llamar la galeria de fotos
 
  $(document).ready(function() {
-     $('.bxSlider').bxSlider({
-         mode: 'fade',
-         captions: true,
-         slideWidth: 1200
-     });
- })
+         $('.bxSlider').bxSlider({
+             mode: 'fade',
+             captions: true,
+             slideWidth: 1200
+         });
+     })
+     // Reloj
+ if (window.location.href.indexOf('reloj') > -1) {
+
+     setInterval(function() {
+         var reloj = moment().format("hh:mm:ss");
+         $('#reloj').html(reloj);
+     }, 1000);
+
+
+ }
